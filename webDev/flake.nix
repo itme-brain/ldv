097624c5fb5db6bdef824b1efb627748e1fcf4a9
Bryan Ramos <bryan@ramos.codes>
2023-04-27 13:02:41 -0400

    removed

--- webDev/flake.nix
+++ webDev/flake.nix
@@ -1,13 +1,10 @@
 {
   description = "Web Development Flake";
-  
-  #TODO: Add more shells, name them each by environment e.g. reactShell 
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
   };
 
-  # Run nix develop --flake '.#<SHELL>' to enter a dev shell
   outputs = { self, nixpkgs }: {
     
     devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; 
