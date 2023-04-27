75cf7dc53ccf6dc39c87c4ebbbe43276e2b73160
Bryan Ramos <bryan@ramos.codes>
2023-04-27 12:44:43 -0400

    Updated

--- webDev/flake.nix
+++ webDev/flake.nix
@@ -1,13 +1,15 @@
 {
-  description = "Simple React Web Development Flake";
+  description = "Web Development Flake";
+  
+  #TODO: Add more shells, name them each by environment e.g. reactShell 
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
   };
 
+  # Run nix develop --flake '.#<SHELL>' to enter a dev shell
   outputs = { self, nixpkgs }: {
     
-    # Run nix develop to enter a dev shell
     devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; 
       mkShell {
         buildInputs = [
