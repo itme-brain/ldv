63226ecf87578d9aa496b602955211bc413f483a
Bryan Ramos <bryan@ramos.codes>
2023-05-23 04:31:22 -0400

    Added

---
+++ haskellDev/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Haskell Development Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    
+    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; 
+      mkShell {
+        buildInputs = [
+          ghc
+          cabal-install
+        ];
+      };
+  };
+}
