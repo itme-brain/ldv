cd8baa67dc7b3068933640fbc0918eb4c4ba8221
Bryan Ramos <bryan@ramos.codes>
2023-05-23 12:06:42 -0400

    Added elixir

--- webDev/flake.nix
+++ webDev/flake.nix
@@ -1,19 +1,36 @@
 {
-  description = "Web Development Flake";
+  description = "A development environment for Elixir and Phoenix";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
-  outputs = { self, nixpkgs }: {
-    
-    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; 
-      mkShell {
-        buildInputs = [
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      elixir = pkgs.elixir_1_14;
+      nodejs = pkgs.nodejs-19_x;
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          elixir
           nodejs
-          python3
+          postgresql_12
         ];
-      };
-  };
-}
+  
+        shellHook = ''
+          if ! mix local.hex --help > /dev/null 2>&1; then
+            mix local.hex
+          fi
 
+          if ! mix local.rebar --help > /dev/null 2>&1; then
+            mix local.rebar
+          fi
+
+          if ! mix archive | grep phx_new > /dev/null 2>&1; then
+            mix archive.install hex phx_new
+          fi
+        '';
+      };
+    };
+}
