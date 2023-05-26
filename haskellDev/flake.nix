e64c8384affa6508920f87e4a2571a5c555b98f7
Bryan Ramos <bryan@ramos.codes>
2023-05-26 10:36:45 -0400

    Changed to stable nixpkgs

--- haskellDev/flake.nix
+++ haskellDev/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A development environment for Haskell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }:
     let
