150a88a2ca6948a6044e52d0bee47e55c195ae20
Bryan Ramos <bryan@ramos.codes>
2023-05-26 15:41:13 -0400

    nvm back to stable lol

--- haskellDev/flake.nix
+++ haskellDev/flake.nix
@@ -2,14 +2,12 @@
   description = "A development environment for Haskell";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-  inputs.usnixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, usnixpkgs }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      upkgs = import usnixpkgs { inherit system; };
-      ghc = upkgs.haskell.compiler.ghc96;
+
     in
     {
       devShells.${system}.default = pkgs.mkShell {
