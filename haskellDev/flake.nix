3ff459467b3cbb1e90f618e60d8180d91e4f7eda
Bryan Ramos <bryan@ramos.codes>
2023-05-26 14:45:13 -0400

    Updated GHC

--- haskellDev/flake.nix
+++ haskellDev/flake.nix
@@ -7,6 +7,7 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      ghc = pkgs.haskell.compiler.ghcHEAD;
     in
     {
       devShell.${system} = pkgs.mkShell {
