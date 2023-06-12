d5da44341efa8e19ad999c01acf47d69635017ca
Bryan Ramos <bryan@ramos.codes>
2023-06-12 17:05:28 -0400

    oopsV2

--- ldv.sh
+++ ldv.sh
@@ -2,8 +2,8 @@
 
 function ldv() {
 
-if [[ $1 == "help" ]] || [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
-  cat << EOF
+	if [[ $1 == "help" ]] || [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+		cat <<EOF
 ldv
 A simple utility for setting up development environments effortlessly.
 Commands:
@@ -14,16 +14,16 @@ Commands:
 Contributions welcome: https://github.com/itme-brain/ldv
 EOF
 
-elif [[ $1 == "init" ]] || [[ $1 == "-i" ]] || [[ $1 == "--init" ]]; then
-  if [[ -e ./shell.nix ]] || [[ -e ./.envrc ]]; then
-    cat << EOF
+	elif [[ $1 == "init" ]] || [[ $1 == "-i" ]] || [[ $1 == "--init" ]]; then
+		if [[ -e ./shell.nix ]] || [[ -e ./.envrc ]]; then
+			cat <<EOF
 Existing environment found.
 Initialization cancelled.
 EOF
-    return
-  fi
-  
-  cat << EOF
+			return
+		fi
+
+		cat <<EOF
 Initializing a new environment...
 Select an environment:
 1. Web
@@ -31,74 +31,75 @@ Select an environment:
 3. Haskell
 4. Python
 EOF
-  # Add more options here...
+		# Add more options here...
 
-  read -p "Enter the number of your choice: " choice
+		read -p "Enter the number of your choice: " choice
 
-  case $choice in
-    1)
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/web.nix -O shell.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
-      direnv allow
-      ;;
-    2)
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/elixir.nix -O shell.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
-      direnv allow
-      ;;
-    3)
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/haskell.nix -O shell.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
-      direnv allow
-      ;;
-    4)
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/python.nix -O shell.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
-      wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
-      direnv allow
-      ;;
-    # Add more cases here...
-    *)
-      echo "Invalid choice."
-      ;;
-  esac
-elif [[ -z $1 ]]; then
-  cat << EOF
+		case $choice in
+		1)
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/web.nix -O shell.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
+			direnv allow
+			;;
+		2)
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/elixir.nix -O shell.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
+			direnv allow
+			;;
+		3)
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/haskell.nix -O shell.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
+			direnv allow
+			;;
+		4)
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/python.nix -O shell.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
+			direnv allow
+			;;
+		# Add more cases here...
+		*)
+			echo "Invalid choice."
+			;;
+		esac
+	elif [[ -z $1 ]]; then
+		cat <<EOF
 Select an environment:
 1. Web
 2. Elixir
 3. Haskell
 4. Python
 EOF
-  # Add more options here...
+		# Add more options here...
 
-  read -p "Enter the number of your choice: " choice
+		read -p "Enter the number of your choice: " choice
 
-  case $choice in
-    1)
-      (nix develop github:itme-brain/ldv#web)
-      ;;
-    2)
-      (nix develop github:itme-brain/ldv#elixir)
-      ;;
-    3)
-      (nix develop github:itme-brain/ldv#haskell)
-      ;;
-    4)
-      (nix develop github:itme-brain/ldv#python)
-    # Add more cases here...
-    *)
-      echo "Invalid choice."
-      ;;
-  esac
-else
-  echo "Error: Invalid command. Try 'ldv --help'"
-fi
+		case $choice in
+		1)
+			(nix develop github:itme-brain/ldv#web)
+			;;
+		2)
+			(nix develop github:itme-brain/ldv#elixir)
+			;;
+		3)
+			(nix develop github:itme-brain/ldv#haskell)
+			;;
+		4)
+			(nix develop github:itme-brain/ldv#python)
+			;;
+		# Add more cases here...
+		*)
+			echo "Invalid choice."
+			;;
+		esac
+	else
+		echo "Error: Invalid command. Try 'ldv --help'"
+	fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  ldv "$@"
+	ldv "$@"
 fi
