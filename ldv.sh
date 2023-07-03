b8bc8b9413c3b7be31e2a92d61d950f28e85c3b0
Bryan Ramos <bryan@ramos.codes>
2023-07-03 14:16:36 -0400

    adjusted web name - added vue shell

--- ldv.sh
+++ ldv.sh
@@ -26,10 +26,11 @@ EOF
 		cat <<EOF
 Initializing a new environment...
 Select an environment:
-1. Web
+1. Purescript
 2. Elixir
 3. Haskell
 4. Python
+5. VueJS
 EOF
 		# Add more options here...
 
@@ -37,7 +38,7 @@ EOF
 
 		case $choice in
 		1)
-			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/web.nix -O shell.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/purs.nix -O shell.nix
 			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
 			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
 			direnv allow
@@ -60,6 +61,12 @@ EOF
 			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
 			direnv allow
 			;;
+		5)
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/shells/vue.nix -O shell.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/flake -O flake.nix
+			wget -q https://raw.githubusercontent.com/itme-brain/ldv/main/utils/envrc -O .envrc
+			direnv allow
+			;;
 		# Add more cases here...
 		*)
 			echo "Invalid choice."
@@ -68,10 +75,11 @@ EOF
 	elif [[ -z $1 ]]; then
 		cat <<EOF
 Select an environment:
-1. Web
+1. Purescript
 2. Elixir
 3. Haskell
 4. Python
+5. VueJS
 EOF
 		# Add more options here...
 
@@ -79,7 +87,7 @@ EOF
 
 		case $choice in
 		1)
-			(nix develop github:itme-brain/ldv#web)
+			(nix develop github:itme-brain/ldv#purs)
 			;;
 		2)
 			(nix develop github:itme-brain/ldv#elixir)
@@ -89,6 +97,9 @@ EOF
 			;;
 		4)
 			(nix develop github:itme-brain/ldv#python)
+			;;
+		5)
+			(nix develop github:itme-brain/ldv#vue)
 			;;
 		# Add more cases here...
 		*)
---
+++ shells/vue.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+# Add more packages to this shell.
+# https://search.nixos.org/packages
+
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    nodejs
+    vue-cli
+  ];
+  shellHook = ''
+    if ! command -v nuxt &> /dev/null
+    then
+        echo "nuxt not found, installing..."
+        npm install nuxt
+    fi
+
+    if ! command -v vite &> /dev/null
+    then
+        echo "vite not found, installing..."
+        npm install create-vite
+    fi
+
+    if ! command -v tailwindcss &> /dev/null
+    then
+        echo "tailwindcss not found, installing..."
+        npm install tailwindcss
+    fi
+  '';
+}
